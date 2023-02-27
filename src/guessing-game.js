5cb7f649bba343e965491c131736c9822a8ee6ca
Inna Barysenka <inna.barysenka@gmail.com>
2023-02-27 19:23:55 +0300

    feat: solving guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+ 
     setRange(min, max) {
-
+this.min = min +1;
+this.max = max -1;
     }
 
     guess() {
-
+return Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+this.max = this.guess() - 1
     }
 
     greater() {
-
+        this.min = this.guess() + 1 
     }
 }
 
